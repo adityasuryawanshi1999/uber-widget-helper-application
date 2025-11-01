a169dd83b09c3159f4ebf65bc59bd17f0662ee80
adityasuryawanshi1999 <adityasuryawanshi1999@gmail.com>
2025-11-01 16:14:01 +0530

    Fixed notch issue

--- src/main/screens/UberWidgetApp.js
+++ src/main/screens/UberWidgetApp.js
@@ -9,11 +9,13 @@ import {
   Linking,
   Alert,
   StyleSheet,
+  Platform,
   StatusBar,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { RadioButton } from "react-native-paper";
 import { MaterialIcons } from "@expo/vector-icons";
+import Constants from "expo-constants"; // gives you the status bar height
 
 export default function UberWidgetApp() {
   const [widgets, setWidgets] = useState([]);
@@ -248,192 +250,209 @@ export default function UberWidgetApp() {
   };
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="#f9fafc" />
-      <Text style={styles.title}>Quick Routes 🚗</Text>
-
-      <TextInput
-        placeholder="Shortcut Name"
-        placeholderTextColor="black"
-        value={newWidget.name}
-        onChangeText={(t) => setNewWidget({ ...newWidget, name: t })}
-        style={styles.input}
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        paddingTop:
+          Platform.OS === "android"
+            ? StatusBar.currentHeight // Android notch/status bar height
+            : Constants.statusBarHeight, // iOS status bar height
+      }}
+    >
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
       />
+      <View style={styles.container}>
+        <Text style={styles.title}>Quick Routes 🚗</Text>
 
-      <View>
         <TextInput
-          placeholder="Enter pickup location"
+          placeholder="Shortcut Name"
           placeholderTextColor="black"
-          value={queryPickup}
-          onChangeText={handlePickupTextChange}
+          value={newWidget.name}
+          onChangeText={(t) => setNewWidget({ ...newWidget, name: t })}
           style={styles.input}
-          ref={pickupTextRef}
-          onBlur={() => handleBlur("pickup")}
-          onFocus={() => handleFocus("pickup")}
-          selection={selectionPickupText}
         />
-        {pickupSuggestions.length > 0 && (
-          <FlatList
-            data={pickupSuggestions}
-            keyExtractor={(item) => item.place_id.toString()}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                style={styles.item}
-                onPress={() => handleSelect(item, "pickup")}
-              >
-                <Text>{item.display_name}</Text>
-              </TouchableOpacity>
-            )}
-          />
-        )}
-      </View>
 
-      <View>
-        <TextInput
-          placeholder="Enter drop location"
-          placeholderTextColor="black"
-          value={queryDroppoff}
-          onChangeText={handleDropoffTextChange}
-          style={styles.input}
-          ref={dropoffTextRef}
-          onBlur={() => handleBlur("dropoff")}
-          onFocus={() => handleFocus("dropoff")}
-          selection={selectionDropoffText}
-        />
-        {dropoffSuggestions.length > 0 && (
-          <FlatList
-            data={dropoffSuggestions}
-            keyboardShouldPersistTaps="always"
-            keyExtractor={(item) => item.place_id.toString()}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                style={styles.item}
-                onPress={() => handleSelect(item, "dropoff")}
-              >
-                <Text>{item.display_name}</Text>
-              </TouchableOpacity>
-            )}
+        <View>
+          <TextInput
+            placeholder="Enter pickup location"
+            placeholderTextColor="black"
+            value={queryPickup}
+            onChangeText={handlePickupTextChange}
+            style={styles.input}
+            ref={pickupTextRef}
+            onBlur={() => handleBlur("pickup")}
+            onFocus={() => handleFocus("pickup")}
+            selection={selectionPickupText}
           />
-        )}
-      </View>
-
-      <TextInput
-        placeholder="Pickup (lat,lng)"
-        placeholderTextColor="black"
-        value={newWidget.pickup}
-        onChangeText={(t) => setNewWidget({ ...newWidget, pickup: t })}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Dropoff (lat,lng)"
-        placeholderTextColor="black"
-        value={newWidget.dropoff}
-        onChangeText={(t) => setNewWidget({ ...newWidget, dropoff: t })}
-        style={styles.input}
-      />
-      <View
-        style={{
-          //   flex: 1,
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <View style={styles.eightyPercentWithMargin}>
-          <Button title="Add Shortcut" onPress={addWidget} />
+          {pickupSuggestions.length > 0 && (
+            <FlatList
+              data={pickupSuggestions}
+              keyExtractor={(item) => item.place_id.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  style={styles.item}
+                  onPress={() => handleSelect(item, "pickup")}
+                >
+                  <Text>{item.display_name}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          )}
         </View>
-        <View style={styles.twentyPercent}>
-          {/* <Button title="Clear" onPress={clearAllFields} /> */}
-          <TouchableOpacity
-            onPress={clearAllFields}
-            style={{
-              backgroundColor: "#636363",
-              flex: 1,
-              borderRadius: 8,
-            }}
-          >
-            <Text
-              style={[{ color: "white", textAlign: "center", margin: "auto" }]}
+
+        <View>
+          <TextInput
+            placeholder="Enter drop location"
+            placeholderTextColor="black"
+            value={queryDroppoff}
+            onChangeText={handleDropoffTextChange}
+            style={styles.input}
+            ref={dropoffTextRef}
+            onBlur={() => handleBlur("dropoff")}
+            onFocus={() => handleFocus("dropoff")}
+            selection={selectionDropoffText}
+          />
+          {dropoffSuggestions.length > 0 && (
+            <FlatList
+              data={dropoffSuggestions}
+              keyboardShouldPersistTaps="always"
+              keyExtractor={(item) => item.place_id.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  style={styles.item}
+                  onPress={() => handleSelect(item, "dropoff")}
+                >
+                  <Text>{item.display_name}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          )}
+        </View>
+
+        <TextInput
+          placeholder="Pickup (lat,lng)"
+          placeholderTextColor="black"
+          value={newWidget.pickup}
+          onChangeText={(t) => setNewWidget({ ...newWidget, pickup: t })}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Dropoff (lat,lng)"
+          placeholderTextColor="black"
+          value={newWidget.dropoff}
+          onChangeText={(t) => setNewWidget({ ...newWidget, dropoff: t })}
+          style={styles.input}
+        />
+        <View
+          style={{
+            //   flex: 1,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <View style={styles.eightyPercentWithMargin}>
+            <Button title="Add Shortcut" onPress={addWidget} />
+          </View>
+          <View style={styles.twentyPercent}>
+            {/* <Button title="Clear" onPress={clearAllFields} /> */}
+            <TouchableOpacity
+              onPress={clearAllFields}
+              style={{
+                backgroundColor: "#636363",
+                flex: 1,
+                borderRadius: 8,
+              }}
             >
-              CLEAR
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.radioContainer}>
-        <View style={styles.radioGroup}>
-          {/* First radio button for ReactJS */}
-          <View style={styles.radioButton}>
-            <RadioButton.Android
-              value="option1"
-              status={
-                selectedCabService === "option1" ? "checked" : "unchecked"
-              }
-              onPress={() => setSelectedCabService("option1")}
-              color="#007BFF" // Custom color for the radio button
-            />
-            <Text style={styles.radioLabel}>Uber</Text>
-          </View>
-
-          {/* Second radio button for NextJs */}
-          <View style={styles.radioButton}>
-            <RadioButton.Android
-              value="option2"
-              status={
-                selectedCabService === "option2" ? "checked" : "unchecked"
-              }
-              onPress={() => setSelectedCabService("option2")}
-              color="#007BFF" // Custom color for the radio button
-            />
-            <Text style={styles.radioLabel}>Ola</Text>
-          </View>
-
-          {/* Second radio button for NextJs */}
-          <View style={styles.radioButton}>
-            <RadioButton.Android
-              value="option3"
-              status={
-                selectedCabService === "option3" ? "checked" : "unchecked"
-              }
-              onPress={() => setSelectedCabService("option3")}
-              color="#007BFF" // Custom color for the radio button
-            />
-            <Text style={styles.radioLabel}>Both</Text>
+              <Text
+                style={[
+                  { color: "white", textAlign: "center", margin: "auto" },
+                ]}
+              >
+                CLEAR
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
-      </View>
-
-      <FlatList
-        data={widgets}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <View style={styles.shortcutContainer}>
-            <View style={styles.eightyPercent}>
-              <TouchableOpacity
-                style={styles.shortcut}
-                onPress={() =>
-                  openCabApps(item.pickup, item.dropoff, item.name)
+        <View style={styles.radioContainer}>
+          <View style={styles.radioGroup}>
+            {/* First radio button for ReactJS */}
+            <View style={styles.radioButton}>
+              <RadioButton.Android
+                value="option1"
+                status={
+                  selectedCabService === "option1" ? "checked" : "unchecked"
                 }
-              >
-                <Text style={styles.shortcutText}>{item.name}</Text>
-              </TouchableOpacity>
+                onPress={() => setSelectedCabService("option1")}
+                color="#007BFF" // Custom color for the radio button
+              />
+              <Text style={styles.radioLabel}>Uber</Text>
             </View>
-            <View style={styles.twentyPercent}>
-              <TouchableOpacity
-                style={styles.deleteBtn}
-                onPress={() => deleteShortcut(index)}
-              >
-                {/* <Text>Delete</Text> */}
-                <MaterialIcons
-                  name="delete"
-                  size={30}
-                  color="white"
-                  style={{ textAlign: "center" }}
-                />
-              </TouchableOpacity>
+
+            {/* Second radio button for NextJs */}
+            <View style={styles.radioButton}>
+              <RadioButton.Android
+                value="option2"
+                status={
+                  selectedCabService === "option2" ? "checked" : "unchecked"
+                }
+                onPress={() => setSelectedCabService("option2")}
+                color="#007BFF" // Custom color for the radio button
+              />
+              <Text style={styles.radioLabel}>Ola</Text>
+            </View>
+
+            {/* Second radio button for NextJs */}
+            <View style={styles.radioButton}>
+              <RadioButton.Android
+                value="option3"
+                status={
+                  selectedCabService === "option3" ? "checked" : "unchecked"
+                }
+                onPress={() => setSelectedCabService("option3")}
+                color="#007BFF" // Custom color for the radio button
+              />
+              <Text style={styles.radioLabel}>Both</Text>
             </View>
           </View>
-        )}
-      />
+        </View>
+
+        <FlatList
+          data={widgets}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <View style={styles.shortcutContainer}>
+              <View style={styles.eightyPercent}>
+                <TouchableOpacity
+                  style={styles.shortcut}
+                  onPress={() =>
+                    openCabApps(item.pickup, item.dropoff, item.name)
+                  }
+                >
+                  <Text style={styles.shortcutText}>{item.name}</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.twentyPercent}>
+                <TouchableOpacity
+                  style={styles.deleteBtn}
+                  onPress={() => deleteShortcut(index)}
+                >
+                  {/* <Text>Delete</Text> */}
+                  <MaterialIcons
+                    name="delete"
+                    size={30}
+                    color="white"
+                    style={{ textAlign: "center" }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -459,7 +478,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#f9fafc",
-    marginTop: 30,
   },
   title: {
     fontSize: 24,
